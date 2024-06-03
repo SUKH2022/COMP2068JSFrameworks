859160a3881c4c1adc7b76cb128b986084a9a540
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2024-06-03 16:07:38 -0400

    Update lab2.js
    update some comments and the outcome of the game

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -6,6 +6,7 @@ const prompt = require('prompt');
 
 // Func. to determine winner based on selected option of the user
 function determineWinner(userSelection, computerSelection) {
+    // decision structure using if-else
     if (userSelection === computerSelection) {
         return "It's a tie!";
     } else if (userSelection === "ROCK") {
@@ -36,7 +37,7 @@ prompt.get(['userSelection'], (err, result) => {
     // To ensure case-insensitive user input
     const userSelection = result.userSelection.toUpperCase(); 
 
-    // Generate random number for the section of the comp.
+    // the Math.random() func. to generate a no. as computerSelection
     const randNum = Math.random();
     let computerSelection;
     if (randNum < 0.34) {
@@ -47,9 +48,12 @@ prompt.get(['userSelection'], (err, result) => {
         computerSelection = "ROCK";
     }
 
-    // Displaying the outcome of the game
+    // Displaying both the userSelection and 
+    // computerSelection variables using console.log()
     console.log(`User Selection: ${userSelection}`);
     console.log(`Computer Selection: ${computerSelection}`);
+    
+    // Displaying the outcome of the game
     console.log(determineWinner(userSelection, computerSelection));
 });
 
@@ -66,4 +70,11 @@ paper
 User Selection: PAPER
 Computer Selection: SCISSORS
 Computer Wins!
+
+[nodemon] starting `node lab2.js`
+prompt: userSelection:  scissors
+scissors
+User Selection: SCISSORS
+Computer Selection: PAPER
+User Wins!
 */
