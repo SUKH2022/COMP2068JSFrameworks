f5f9eda4428e868d1612ce16b5c34316d58394fd
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2024-08-03 11:18:36 -0400

    removing some stuffs

--- ASSIGNMENT02/config/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.CONNECTION_STRING_MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
--- ASSIGNMENT02/routes/blogRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const blogController = require('../controllers/blogController');
-
-router.get('/', blogController.homePage);
-
-module.exports = router;
