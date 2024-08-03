884d592eccb09da4f041fed96820c193d9b70cf6
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2024-08-03 12:04:58 -0400

    updated package.json, index and routers

---
+++ ASSIGNMENT02/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'));
+
+app.listen(PORT, ()=> {
+    console.log('App listening to port ${PORT}');
+})
