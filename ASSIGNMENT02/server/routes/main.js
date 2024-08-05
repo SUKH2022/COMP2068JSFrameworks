076f95e1ddbbb475b292a63b7e0432fccdacaec6
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2024-08-05 09:50:14 -0400

    added home and about pages

--- ASSIGNMENT02/server/routes/main.js
+++ ASSIGNMENT02/server/routes/main.js
@@ -3,7 +3,22 @@ const router = express.Router();
 
 // router
 router.get('', (req,res)=>{
-  res.render('index');
+  const locals = {
+    title: "Blog Website",
+    description: "Blog created with NodeJs, Express, & MongoDb."
+    }  
+    res.render('index', { 
+      locals
+    });
+});
+
+/**
+ * GET / About
+*/
+router.get('/about', (req, res) => {
+  res.render('about', {
+    // currentRoute: '/about'
+  });
 });
 
 module.exports = router;
