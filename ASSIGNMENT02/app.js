7e6a7d70d0df1ea2fe2890d76e68c5f158c1d457
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2024-08-05 12:40:30 -0400

    added admin

--- ASSIGNMENT02/app.js
+++ ASSIGNMENT02/app.js
@@ -15,6 +15,21 @@ const PORT = 5000 || process.env.PORT;
 // Connect to DB
 connectDB();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}));
+
 app.use(express.static('public'));
 
 // Templating Engine
@@ -23,6 +38,7 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 app.listen(PORT, ()=> {
     console.log('App listening to port ${PORT}');
